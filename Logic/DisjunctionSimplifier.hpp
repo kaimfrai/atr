3d3838745f4615a0426e2d658e517ece23157f8e
Kai Müller <kmuell23@hs-mittweida.de>
2021-10-19 16:15:33 +0200

    added new files after refactoring

---
+++ Logic/DisjunctionSimplifier.hpp
@@ -0,0 +1,196 @@
+#pragma once
+
+#include "RedundantClauseFilter.hpp"
+#include "MakeTerm.hpp"
+#include "Identity.hpp"
+#include "Types.hpp"
+#include "Concepts.hpp"
+
+template
+	<	ProtoClause
+		...	t_tpOldClause
+	>
+struct
+	DisjunctionSimplifier
+{
+	explicit consteval
+	(	DisjunctionSimplifier
+	)	(	t_tpOldClause
+			...
+		)
+	{}
+
+	explicit consteval
+	(	DisjunctionSimplifier
+	)	(	Or<t_tpOldClause...>
+		)
+	{}
+
+	auto consteval
+	(	operator()
+	)	(	True
+		)	const
+	->	True
+	{	return {};	}
+
+	auto consteval
+	(	operator()
+	)	(	False
+		)	const
+	->	ProtoTerm auto
+	{	return
+		Disjunction
+		(	t_tpOldClause{}
+			...
+		);
+	}
+
+	static auto consteval
+	(	FilterOldClauses
+	)	(	ProtoClause auto
+				i_vNewClause
+		)
+	->	ProtoTerm auto
+	{
+		ProtoTerm auto constexpr
+			vUnsimplified
+		=	Disjunction
+			(	t_tpOldClause{}
+				...
+			)
+		;
+		ProtoTerm auto constexpr
+			vSimplified
+		=(	SelfIgnoringRedundantClauseFilter
+			{	t_tpOldClause{}
+			}(	i_vNewClause
+			,	t_tpOldClause{}
+				...
+			)
+		or	...
+		);
+
+		if	constexpr
+			(	vSimplified
+			==	vUnsimplified
+			)
+			return
+			Disjunction
+			(	i_vNewClause
+			,	t_tpOldClause{}
+				...
+			);
+		else
+			return
+				i_vNewClause
+			or	vSimplified
+			;
+	}
+
+
+	auto consteval
+	(	operator()
+	)	(	ProtoClause auto
+				i_vNewClause
+		)	const
+	->	ProtoTerm auto
+	{
+		ProtoClause auto constexpr
+			vSimplified
+		=	RedundantClauseFilter
+			{	i_vNewClause
+			}(	t_tpOldClause{}
+				...
+			)
+		;
+
+		if	constexpr
+			(	vSimplified
+			==	i_vNewClause
+			)
+			return
+			FilterOldClauses
+			(	i_vNewClause
+			);
+		else
+			return
+			operator()
+			(	vSimplified
+			);
+	}
+
+
+};
+
+template
+	<>
+struct
+	DisjunctionSimplifier
+	<	True
+	>
+{
+	explicit consteval
+	(	DisjunctionSimplifier
+	)	(	True
+		)
+	{}
+
+	auto consteval
+	(	operator()
+	)	(	ProtoClause auto
+		)	const
+	->	True
+	{	return {};	}
+};
+
+template
+	<>
+struct
+	DisjunctionSimplifier
+	<	False
+	>
+{
+	explicit consteval
+	(	DisjunctionSimplifier
+	)	(	False
+		)
+	{}
+
+	auto consteval
+	(	operator()
+	)	(	ProtoClause auto
+				i_vNewClause
+		)	const
+	->	ProtoClause auto
+	{	return
+			i_vNewClause
+		;
+	}
+};
+
+template
+	<	ProtoClause
+		...	t_tpOldClause
+	>
+(	DisjunctionSimplifier
+)	(	t_tpOldClause
+		...
+	)
+->	DisjunctionSimplifier
+	<	t_tpOldClause
+		...
+	>
+;
+
+template
+	<	ProtoClause
+		...	t_tpOldClause
+	>
+(	DisjunctionSimplifier
+)	(	Or<t_tpOldClause...>
+	)
+->	DisjunctionSimplifier
+	<	t_tpOldClause
+		...
+	>
+;
---
+++ Logic/RedundantClauseFilter.hpp
@@ -0,0 +1,180 @@
+#pragma once
+
+#include "RedundantLiteralFilter.hpp"
+#include "Substitute.hpp"
+#include "Identity.hpp"
+#include "Types.hpp"
+#include "Concepts.hpp"
+
+///	Filters Clauses which imply the Disjunction of all other clauses.
+///	For example:
+///		(A and	B and C)	or	(A and B)
+///	=>	(False)				or	(A and B)
+///		(A and B)	or	(A and C)	or	(not B and C)
+///	=>	(A and B)	or	(False)		or	(not B and C)
+template
+	<	ProtoLiteral
+		...	t_tpLiteral
+	>
+struct
+	RedundantClauseFilter
+{
+	explicit consteval
+	(	RedundantClauseFilter
+	)	(	t_tpLiteral
+			...
+		)
+	{}
+
+	explicit consteval
+	(	RedundantClauseFilter
+	)	(	And<t_tpLiteral...>
+		)
+	{}
+
+	static auto consteval
+	(	IsClauseFalse
+	)	()
+	->	bool
+	{
+		return
+		IsFalse
+		((	t_tpLiteral{}
+		and	...
+		));
+	}
+
+	static auto consteval
+	(	AssumeClauseTrue
+	)	(	ProtoClause auto
+			...	i_vpClause
+		)
+	->	bool
+	{
+		auto constexpr
+			fAssumeLiteralsTrue
+		=	SubstituteTrue
+			(	t_tpLiteral{}
+				...
+			)
+		;
+		return
+		IsTrue
+		((	fAssumeLiteralsTrue
+			(	i_vpClause
+			)
+		or	...
+		));
+	}
+
+	auto consteval
+	(	operator()
+	)	(	ProtoClause auto
+			...	i_vpClause
+		)	const
+	->	ProtoClause auto
+	{
+		if	constexpr
+			(	//	False => AnyTerm
+				IsClauseFalse()
+			or	//	True => True
+				AssumeClauseTrue
+				(	i_vpClause
+					...
+				)
+			)
+			//	cancel this clause
+			return
+			False
+			{};
+		else
+			//	filter literals within this clause
+			return
+			(	RedundantLiteralFilter
+				{	t_tpLiteral{}
+				,	t_tpLiteral{}
+					...
+				}(	i_vpClause
+					...
+				)
+			and	...
+			);
+	}
+};
+
+template
+	<	ProtoLiteral
+		...	t_tpLiteral
+	>
+(	RedundantClauseFilter
+)	(	t_tpLiteral
+		...
+	)
+->	RedundantClauseFilter
+	<	t_tpLiteral
+		...
+	>
+;
+
+template
+	<	ProtoLiteral
+		...	t_tpLiteral
+	>
+(	RedundantClauseFilter
+)	(	And<t_tpLiteral...>
+	)
+->	RedundantClauseFilter
+	<	t_tpLiteral
+		...
+	>
+;
+
+template
+	<	ProtoClause
+			t_tFilterClause
+	>
+struct
+	SelfIgnoringRedundantClauseFilter
+{
+	consteval
+	(	SelfIgnoringRedundantClauseFilter
+	)	(	t_tFilterClause
+		)
+	{}
+
+	auto consteval
+	(	operator()
+	)	(	ProtoClause auto
+			...	i_vpClause
+		)	const
+	->	ProtoClause auto
+	{
+		auto constexpr
+			fRemoveFilterClause
+		=	SubstituteFalse
+			(	t_tFilterClause{}
+			)
+		;
+
+		return
+		RedundantClauseFilter
+		{	t_tFilterClause{}
+		}(	fRemoveFilterClause
+			(	i_vpClause
+			)
+			...
+		);
+	}
+};
+
+template
+	<	ProtoClause
+			t_tFilterClause
+	>
+(	SelfIgnoringRedundantClauseFilter
+)	(	t_tFilterClause
+	)
+->	SelfIgnoringRedundantClauseFilter
+	<	t_tFilterClause
+	>
+;
