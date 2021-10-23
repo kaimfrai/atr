b3175a1858a908d65fc08155466c52cf61b214d0
Kai Müller <kmuell23@hs-mittweida.de>
2021-10-23 21:59:10 +0200

    added Clause x Literal tests

---
+++ Logic/Test/Clause_and_Literal.cpp
@@ -0,0 +1,41 @@
+#include "Shared.hpp"
+
+/// ****************************************************************************
+///	(p)
+/// ****************************************************************************
+static_assert
+(	((p		and	q)			and	p)	==	(p	and	q)
+);
+static_assert
+(	((!p	and	q)			and	p)	==	(F)
+);
+static_assert
+(	((q		and	r)			and	p)	==	(q	and	r	and	p)
+);
+
+static_assert
+(	((p		and	q	and	r)	and	p)	==	(p	and	q	and	r)
+);
+static_assert
+(	((!p	and	q	and	r)	and	p)	==	(F)
+);
+
+/// ****************************************************************************
+///	(!p)
+/// ****************************************************************************
+static_assert
+(	((p		and	q)			and	!p)	==	(F)
+);
+static_assert
+(	((!p	and	q)			and	!p)	==	(!p	and	q)
+);
+static_assert
+(	((q		and	r)			and	!p)	==	(q	and	r	and	!p)
+);
+
+static_assert
+(	((p		and	q	and	r)	and	!p)	==	(F)
+);
+static_assert
+(	((!p	and	q	and	r)	and	!p)	==	(!p	and	q	and	r)
+);
---
+++ Logic/Test/Clause_or_Literal.cpp
@@ -0,0 +1,41 @@
+#include "Shared.hpp"
+
+/// ****************************************************************************
+///	(p)
+/// ****************************************************************************
+static_assert
+(	((p		and	q)			or	p)	==	(p)
+);
+static_assert
+(	((!p	and	q)			or	p)	==	(q	or	p)
+);
+static_assert
+(	((q		and	r)			or	p)	==	(q	and	r	or	p)
+);
+
+static_assert
+(	((p		and	q	and	r)	or	p)	==	(p)
+);
+static_assert
+(	((!p	and	q	and	r)	or	p)	==	(q	and	r	or	p)
+);
+
+/// ****************************************************************************
+///	(!p)
+/// ****************************************************************************
+static_assert
+(	((p		and	q)			or	!p)	==	(q	or	!p)
+);
+static_assert
+(	((!p	and	q)			or	!p)	==	(!p)
+);
+static_assert
+(	((q		and	r)			or	!p)	==	(q	and	r	or	!p)
+);
+
+static_assert
+(	((p		and	q	and	r)	or	!p)	==	(q	and	r	or	!p)
+);
+static_assert
+(	((!p	and	q	and	r)	or	!p)	==	(!p)
+);
