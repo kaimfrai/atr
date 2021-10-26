3ca57585e63e0238811eb4881c9394d3ec3797c4
Kai Müller <kmuell23@hs-mittweida.de>
2021-10-26 13:54:17 +0200

    removed dependency on identity header in equivalence

--- Logic/Equivalence.hpp
+++ Logic/Equivalence.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
-#include "Identity.hpp"
 #include "Implication.hpp"
 #include "Concepts.hpp"
+
+template
+	<	ProtoTerm
+			t_tTerm
+	>
+auto consteval
+(	operator <=>
+)	(	t_tTerm
+	,	t_tTerm
+	)
+->	bool
+{	return true;	}
+
 
 auto consteval
 (	operator <=>
@@ -13,20 +25,12 @@ auto consteval
 	)
 ->	bool
 {
-	if	constexpr
-		(	i_vLeft
-		==	i_vRight
+	return
+	(	(	i_vLeft
+		>=	i_vRight
 		)
-		return
-			true
-		;
-	else
-		return
-		(	(	i_vLeft
-			>=	i_vRight
-			)
-		and	(	i_vLeft
-			<=	i_vRight
-			)
-		);
+	and	(	i_vLeft
+		<=	i_vRight
+		)
+	);
 }
