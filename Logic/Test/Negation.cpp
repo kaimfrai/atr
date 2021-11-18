6aa5d3e6f2180b2e7da00b541da61fa221bb35d0
Kai Müller <kmuell23@hs-mittweida.de>
2021-11-18 18:57:58 +0100

    added alternative term representations to negation tests

--- Logic/Test/Negation.cpp
+++ Logic/Test/Negation.cpp
@@ -186,6 +186,41 @@ static_assert
 ==	(p	and	!q	and	!r	or	!p	and	q)
 );
 
+
+///	Alternative representation
+static_assert
+(	not
+	(p	and	q	or	!p	and	!q	or	!q	and	r)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+static_assert
+(	not
+	(p	and	q	or	!q	and	r	or	!p	and	!q)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+
+static_assert
+(	not
+	(!p	and	!q	or	p	and	q	or	!q	and	r)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+static_assert
+(	not
+	(!p	and	!q	or	!q	and	r	or	p	and	q)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+
+static_assert
+(	not
+	(!q	and	r	or	p	and	q	or	!p	and	!q)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+static_assert
+(	not
+	(!q	and	r	or	!p	and	!q	or	p	and	q)
+==	(p	and	!q	and	!r	or	!p	and	q)
+);
+
 /// ************************************************************************************************
 ///	(p	and	q	or	p	and	r	or	q	and	r)
 /// ************************************************************************************************
@@ -319,6 +354,40 @@ static_assert
 static_assert
 (	not
 	(!q	and	!r	or	!p	and	r	or	p	and	q)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+
+///	Alternative representation
+static_assert
+(	not
+	(p	and	!r	or	!p	and	!q	or	q	and	r)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+static_assert
+(	not
+	(p	and	!r	or	q	and	r	or	!p	and	!q)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+
+static_assert
+(	not
+	(!p	and	!q	or	p	and	!r	or	q	and	r)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+static_assert
+(	not
+	(!p	and	!q	or	q	and	r	or	p	and	!r)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+
+static_assert
+(	not
+	(q	and	r	or	p	and	!r	or	!p	and	!q)
+==	(p	and	!q	and	r	or	!p	and	q	and	!r)
+);
+static_assert
+(	not
+	(q	and	r	or	!p	and	!q	or	p	and	!r)
 ==	(p	and	!q	and	r	or	!p	and	q	and	!r)
 );
 
