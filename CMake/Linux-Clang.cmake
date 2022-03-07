ef5dbfe89ace13194a4fe792e0eb7639301a6651
Kai Müller <kmuell23@hs-mittweida.de>
2022-03-07 20:10:43 +0100

    Commit to Clang-15

--- CMake/Linux-Clang.cmake
+++ CMake/Linux-Clang.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_C_COMPILER "clang-15")
-set(CMAKE_CXX_COMPILER "clang++-15")
+set(CMAKE_CXX_COMPILER "clang++-15") # required for module partitions
 
 set(CXX_STANDARD_VERSION_FLAG
 	-std=c++2b
