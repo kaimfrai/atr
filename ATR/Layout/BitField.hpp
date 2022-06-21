480dc3c830f0a8e6527c05ca291c35317ad1361b
Kai Müller <kmuell23@hs-mittweida.de>
2022-06-21 21:52:06 +0200

    Make helper functions module local

--- ATR/Layout/BitField.hpp
+++ ATR/Layout/BitField.hpp
@@ -261,40 +261,6 @@ export namespace
 		}
 	};
 
-
-	template
-		<	Meta::USize
-				t_nSize
-		>
-	auto constexpr
-	(	BitFieldOffsetArray
-	)	(	::std::array<::Meta::USize, t_nSize> const
-			&	i_rSizes
-		)
-	->	::std::array<USize, t_nSize>
-	{
-		::std::array<USize, t_nSize>
-			vOffsetArray
-		{};
-
-		::std::transform
-		(	begin(i_rSizes)
-		,	end(i_rSizes)
-		,	begin(vOffsetArray)
-		,	[	m_nOffset = 0uz
-			]	(	USize
-						i_nSize
-				)	mutable
-			->	USize
-			{
-				auto const nCurrent = m_nOffset;
-				m_nOffset += i_nSize;
-				return nCurrent;
-			}
-		);
-		return vOffsetArray;
-	}
-
 	template
 		<	typename
 			...	t_tpBitView
@@ -372,77 +338,110 @@ export namespace
 			);
 		}
 	};
+}
 
-	template
-		<	USize
-				t_nOffset
-		,	typename
-				t_tData
-		,	char const
-			&
-			...	t_rpName
-		>
-	auto constexpr
-	(	MakeBitView
-	)	(	::Member<t_tData, t_rpName...>
-		)
-	->	BitView
-		<	t_nOffset
-		,	t_tData
-		,	t_rpName
-			...
-		>
-	;
+template
+	<	Meta::USize
+			t_nSize
+	>
+auto constexpr
+(	BitFieldOffsetArray
+)	(	::std::array<::Meta::USize, t_nSize> const
+		&	i_rSizes
+	)
+->	::std::array<USize, t_nSize>
+{
+	::std::array<USize, t_nSize>
+		vOffsetArray
+	{};
 
-	template
-		<	USize
-				t_nOffset
-		,	typename
-				t_tMember
-		>
-	using
-		DeduceBitView
-	=	decltype
-		(	MakeBitView
-			<	t_nOffset
-			>(	t_tMember{}
-			)
-		)
-	;
-
-	template
-		<	typename
-			...	t_tpBitFieldMember
-		>
-	auto constexpr
-	(	MakeBitLayout
-	)	()
-	{
-		auto constexpr
-			vOffsetArray
-		=	BitFieldOffsetArray
-			(	{	t_tpBitFieldMember::BitSize
-					...
-				}
-			)
-		;
-
-		return
-		[	&vOffsetArray
-		]	<	USize
-				...	t_npIndex
-			>(	IndexToken<t_npIndex...>
-			)
+	::std::transform
+	(	begin(i_rSizes)
+	,	end(i_rSizes)
+	,	begin(vOffsetArray)
+	,	[	m_nOffset = 0uz
+		]	(	USize
+					i_nSize
+			)	mutable
+		->	USize
 		{
-			return
-			BitLayout
-			<	DeduceBitView
-				<	vOffsetArray[t_npIndex]
-				,	t_tpBitFieldMember
-				>
+			auto const nCurrent = m_nOffset;
+			m_nOffset += i_nSize;
+			return nCurrent;
+		}
+	);
+	return vOffsetArray;
+}
+
+template
+	<	USize
+			t_nOffset
+	,	typename
+			t_tData
+	,	char const
+		&
+		...	t_rpName
+	>
+auto constexpr
+(	MakeBitView
+)	(	::Member<t_tData, t_rpName...>
+	)
+->	ATR::BitView
+	<	t_nOffset
+	,	t_tData
+	,	t_rpName
+		...
+	>
+;
+
+template
+	<	USize
+			t_nOffset
+	,	typename
+			t_tMember
+	>
+using
+	DeduceBitView
+=	decltype
+	(	MakeBitView
+		<	t_nOffset
+		>(	t_tMember{}
+		)
+	)
+;
+
+template
+	<	typename
+		...	t_tpBitFieldMember
+	>
+auto constexpr
+(	MakeBitLayout
+)	()
+{
+	auto constexpr
+		vOffsetArray
+	=	BitFieldOffsetArray
+		(	{	t_tpBitFieldMember::BitSize
 				...
-			>{};
-		}(	Sequence<sizeof...(t_tpBitFieldMember)>
-		);
-	}
+			}
+		)
+	;
+
+	return
+	[	&vOffsetArray
+	]	<	USize
+			...	t_npIndex
+		>(	IndexToken<t_npIndex...>
+		)
+	{
+		return
+		ATR::BitLayout
+		<	DeduceBitView
+			<	vOffsetArray[t_npIndex]
+			,	t_tpBitFieldMember
+			>
+			...
+		>{};
+	}(	Sequence<sizeof...(t_tpBitFieldMember)>
+	);
 }
