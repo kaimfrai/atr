a95c82d3fc63569f5630da7da5417b50fcc6c934
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2022-12-19 18:11:25 +0100

    Add missing Functional export import to Meta

--- Meta/Meta.hpp
+++ Meta/Meta.hpp
@@ -3,6 +3,7 @@ export module Meta;
 export import Meta.Arithmetic;
 export import Meta.Data;
 export import Meta.Dispatch;
+export import Meta.Functional;
 export import Meta.ID;
 export import Meta.Lex;
 export import Meta.Logic;
