8a4eba948ca0cf84fc5bb890e4a5a41d0341932b
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2022-12-21 17:31:02 +0100

    [Evaluation] Fix dyno assembly generation

--- Evaluation/assembly_all.sh
+++ Evaluation/assembly_all.sh
@@ -8,7 +8,7 @@ clang++-16 -std=c++2b -stdlib=libc++ -O3 -fprebuilt-module-path=../build/Evaluat
 echo "any"
 clang++-16 -std=c++2b -stdlib=libc++ -O3 -fprebuilt-module-path=../build/Evaluation/modules -frelaxed-template-template-args -S -masm=intel -fverbose-asm Any/Main.cpp -o ../build/assembly/any.s -fexperimental-library
 echo "dyno"
-clang++-16 -std=c++2b -stdlib=libc++ -O3 -fprebuilt-module-path=../build/Evaluation/modules-frelaxed-template-template-args -S -masm=intel -fverbose-asm Dyno/Main.cpp -o ../build/assembly/dyno.s -fexperimental-library
+clang++-16 -std=c++2b -stdlib=libc++ -O3 -fprebuilt-module-path=../build/Evaluation/modules -frelaxed-template-template-args -S -masm=intel -fverbose-asm Dyno/Main.cpp -o ../build/assembly/dyno.s -fexperimental-library
 echo "polymorphic"
 clang++-16 -std=c++2b -stdlib=libc++ -O3 -fprebuilt-module-path=../build/Evaluation/modules -frelaxed-template-template-args -S -masm=intel -fverbose-asm Polymorphic/Main.cpp -o ../build/assembly/polymorphic.s -fexperimental-library
 echo "type_erasure"
