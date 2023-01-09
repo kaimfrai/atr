a7f150ad30ddf7bdb257b015a7c2fb9bf46377e0
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-01-09 17:19:17 +0100

    [Logic] Move Clause x ??? test cases

---
+++ Test/Meta/Logic/Clause_or_Term/p_and_q.cpp
@@ -0,0 +1,132 @@
+import Meta.Logic.Test.Data;
+
+/// ************************************************************************************************
+///	(p	and	q)
+/// ************************************************************************************************
+static_assert
+(	(	(p	and	q)
+	or	(p	and	q)
+	)
+==	(p	and	q)
+);
+static_assert
+(	(	(p	and	q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q)
+);
+static_assert
+(	(	(p	and	q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q)
+);
+
+static_assert
+(	(	(p	and	!q)
+	or	(p	and	q)
+	)
+==	(p)
+);
+static_assert
+(	(	(p	and	!q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	p	and	r)
+);
+static_assert
+(	(	(p	and	!q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	p	and	!r)
+);
+
+static_assert
+(	(	(p	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	p	and	r)
+);
+static_assert
+(	(	(p	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	p	and	!r)
+);
+
+static_assert
+(	(	(!p	and	q)
+	or	(p	and	q)
+	)
+==	(q)
+);
+static_assert
+(	(	(!p	and	q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	q	and	r)
+);
+static_assert
+(	(	(!p	and	q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	q	and	!r)
+);
+
+static_assert
+(	(	(!p	and	!q)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!p	and	!q)
+);
+static_assert
+(	(	(!p	and	!q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!p	and	!q	and	r)
+);
+static_assert
+(	(	(!p	and	!q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!p	and	!q	and	!r)
+);
+
+static_assert
+(	(	(!p	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!p	and	r)
+);
+static_assert
+(	(	(!p	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!p	and	!r)
+);
+
+static_assert
+(	(	(q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	q	and	r)
+);
+static_assert
+(	(	(q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	q	and	!r)
+);
+
+static_assert
+(	(	(!q	and	r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!q	and	r)
+);
+static_assert
+(	(	(!q	and	!r)
+	or	(p	and	q)
+	)
+==	(p	and	q	or	!q	and	!r)
+);
