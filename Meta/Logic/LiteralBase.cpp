1a8cf8b1463fa2f684955f611b1d8d4bd730d10a
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-01-16 16:15:28 +0100

    [Logic] Remove obsolete Not Trait

--- Meta/Logic/LiteralBase.cpp
+++ Meta/Logic/LiteralBase.cpp
@@ -2,90 +2,35 @@ export module Meta.Logic.LiteralBase;
 
 export import Meta.Token;
 
-import Std;
-
 export namespace
 	Meta::Trait
 {
-	template
-		<	typename
-				t_tLiteral
-		>
-	struct
-		Not final
-	{
-		t_tLiteral
-			Literal
-			[[no_unique_address]]
-		;
-
-		friend auto constexpr
-		(	Evaluate
-		)	(	Not
-					i_vNot
-			,	EraseTypeToken
-					i_vType
-			)
-		->	bool
-		{	return not Evaluate(i_vNot.Literal, i_vType);	}
-
-		template
-			<	typename
-					t_tEntity
-			>
-		auto constexpr
-		(	operator()
-		)	(	TypeToken<t_tEntity>
-					i_vType
-			)	const
-		->	bool
-		{	return not Literal(i_vType);	}
-
-		friend auto constexpr
-		(	operator not
-		)	(	Not
-					i_vNot
-			)
-		->	t_tLiteral
-		{	return i_vNot.Literal;	}
-	};
-
 	struct
 		LiteralBase
 	{
+		[[nodiscard]]
 		friend auto constexpr
 		(	Evaluate
 		)	(	LiteralBase
 			,	EraseTypeToken
 			)
+			noexcept
 		->	bool
-		{	return false;	}
+		{	return
+				false
+			;
+		}
 
-		auto constexpr
+		[[nodiscard]]
+		static auto constexpr
 		(	operator()
 		)	(	EraseTypeToken
-			)	const
-		->	bool
-		{	return false;	}
-
-		template
-			<	::std::derived_from<LiteralBase>
-					t_tLiteral
-			>
-		friend auto constexpr
-		(	operator not
-		)	(	t_tLiteral
-					i_vLiteral
 			)
-		->	Not<t_tLiteral>
-		{	return { i_vLiteral };	}
+			noexcept
+		->	bool
+		{	return
+				false
+			;
+		}
 	};
-
-	Not<LiteralBase> extern
-		Tautology
-	;
-
-	LiteralBase extern
-		Contradiction
-	;
 }
