90ce4c165c6de8b18a4ee7639ace24b4f8bdf229
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-01-24 14:29:34 +0100

    [Trait] Remove BoundedArray definition from UnboundedArray

--- Meta/Trait/Match/UnboundedArray.cpp
+++ Meta/Trait/Match/UnboundedArray.cpp
@@ -8,33 +8,6 @@ export namespace
 	Meta::Trait::Match
 {
 	struct
-		BoundedArray
-	{
-		template
-			<	typename
-					t_tElement
-			,	USize
-					t_nExtent
-			,	typename
-				...	t_tpQualifier
-			>
-		[[nodiscard]]
-		friend auto constexpr
-		(	Evaluate
-		)	(	LiteralBase
-			,	Lex::MatchCVArray
-				<	t_tElement
-				,	t_nExtent
-				,	t_tpQualifier
-					...
-				>
-			)
-			noexcept
-		->	bool
-		{	return t_nExtent != 0uz;	}
-	};
-
-	struct
 		UnboundedArray
 	{
 		template
