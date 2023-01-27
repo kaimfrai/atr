7da6619622634df0761c96d7a034b896b20d337f
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-01-27 17:22:13 +0100

    [Data] Add Aggregate submodules to umbrella module

--- Meta/Data.cpp
+++ Meta/Data.cpp
@@ -1,6 +1,15 @@
 export module Meta.Data;
 
 export import Meta.Data.Aggregate;
+export import Meta.Data.Aggregate.Array;
+export import Meta.Data.Aggregate.BoundedArray;
+export import Meta.Data.Aggregate.CV;
+export import Meta.Data.Aggregate.Function;
+export import Meta.Data.Aggregate.Pointer;
+export import Meta.Data.Aggregate.Primary;
+export import Meta.Data.Aggregate.Reference;
+export import Meta.Data.Aggregate.UnboundedArray;
+export import Meta.Data.Aggregate.Void;
 export import Meta.Data.Object;
 export import Meta.Data.TupleList;
 export import Meta.Data.Value;
