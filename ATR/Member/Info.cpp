c966d1ed79cc47efa6ea8068dc1ec388a5d1cdb2
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-02-03 16:37:54 +0100

    [ATR] Add BitSize to Member.Info

--- ATR/Member/Info.cpp
+++ ATR/Member/Info.cpp
@@ -1,5 +1,8 @@
 export module ATR.Member.Info;
 
+import Meta.Memory.Size;
+import Meta.Memory.Size.Compare;
+import Meta.Trait.BitSize;
 import Meta.Token.Type;
 import Meta.Token.Type.Compare;
 import Meta.ID.StringView;
@@ -7,6 +10,9 @@ import Meta.Size;
 
 import Std;
 
+using ::Meta::TypeToken;
+using ::Meta::BitSize;
+using ::Meta::BitSize_Of;
 using ::Meta::StringView;
 using ::Meta::TypeID;
 using ::Meta::USize;
@@ -15,11 +21,83 @@ export namespace
 	ATR::Member
 {
 	struct
+		TypeInfo
+	{
+		BitSize Size;
+		TypeID Type;
+
+		explicit(false) constexpr
+		(	TypeInfo
+		)	()
+			noexcept
+		=	default;
+
+		template
+			<	typename
+					t_tEntity
+			>
+		explicit(false) constexpr
+		(	TypeInfo
+		)	(	TypeToken
+				<	t_tEntity
+				>	i_vType
+			)
+		:	Size
+			{	BitSize_Of
+				(	i_vType
+				)
+			}
+		,	Type
+			{	i_vType.operator TypeID()
+			}
+		{}
+
+		[[nodiscard]]
+		explicit(false) constexpr
+		(	operator TypeID
+		)	()	const
+			noexcept
+		{	return
+				Type
+			;
+		}
+
+		template
+			<	typename
+					t_tEntity
+			>
+		auto constexpr
+		(	operator =
+		)	(	TypeToken
+				<	t_tEntity
+				>	i_vType
+			)	&
+			noexcept
+		->	TypeInfo&
+		{
+			Size = BitSize_Of(i_vType);
+			Type = i_vType;
+			return
+				*this
+			;
+		}
+
+		[[nodiscard]]
+		friend auto constexpr
+		(	operator <=>
+		)	(	TypeInfo const&
+			,	TypeInfo const&
+			)
+			noexcept
+		=	default;
+	};
+
+	struct
 		Info final
 	{
 		USize SortKey;
 		StringView Name;
-		TypeID Type;
+		TypeInfo Type;
 
 		[[nodiscard]]
 		friend auto constexpr
