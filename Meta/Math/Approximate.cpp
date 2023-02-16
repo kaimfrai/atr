d324b7b15eed82f71310ada03dcca92a60173047
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-02-16 13:12:48 +0100

    [Math] Build Approximation around <=> operator

--- Meta/Math/Approximate.cpp
+++ Meta/Math/Approximate.cpp
@@ -1,59 +1,87 @@
 export module Meta.Math.Approximate;
 
-import Meta.Math.Abs;
-
 import Std;
 
 export namespace
 	Meta::Math
 {
+	template
+		<	::std::floating_point
+				t_tFloat
+		>
 	[[nodiscard]]
 	auto constexpr
 	(	Approximate
-	)	(	::std::floating_point auto
+	)	(	t_tFloat
 				i_nLeft
-		,	decltype(i_nLeft)
+		,	t_tFloat
 				i_nRight
+		,	t_tFloat
+				i_nEpsilon
+			=	::std::numeric_limits
+				<	t_tFloat
+				>
+			::	epsilon()
 		)
 		noexcept
 	->	bool
 	{
 		auto const
-			nAbsLeft
-		=	Abs
-			(	i_nLeft
-			)
+			fCompare
+		=	[	i_nEpsilon
+			]	(	t_tFloat
+						i_nMax
+				,	t_tFloat
+						i_nMin
+				)
+			{	auto const
+					nDifference
+				=	i_nMax
+				-	i_nMin
+				;
+
+				// if min is negative, -min will always be greater than max
+				// otherwise min is positive so max is also positive
+				auto const
+					nMaxAbs
+				=	(	-i_nMin
+					>	i_nMax
+					)
+				?	-i_nMin
+				:	i_nMax
+				;
+
+				return
+					nDifference
+				<=	(	nMaxAbs
+					*	i_nEpsilon
+					)
+				;
+			}
 		;
+
 		auto const
-			nAbsRight
-		=	Abs
+			vCompare
+		=	i_nLeft
+		<=>	i_nRight
+		;
+
+		if	(::std::is_lt(vCompare))
+			return
+			fCompare
 			(	i_nRight
-			)
-		;
-
-		auto const
-		[	nAbsMin
-		,	nAbsMax
-		]=	::std::minmax
-			(	nAbsLeft
-			,	nAbsRight
-			)
-		;
-
-		auto const
-			nDifference
-		=	nAbsMax
-		-	nAbsMin
-		;
+			,	i_nLeft
+			);
+		if	(::std::is_gt(vCompare))
+			return
+			fCompare
+			(	i_nLeft
+			,	i_nRight
+			);
 
 		return
-			nDifference
-		<=	(	nAbsMin
-			*	::std::numeric_limits
-				<	decltype(i_nLeft)
-				>
-			::	epsilon()
-			)
-		;
+		::std::is_eq
+		(	vCompare
+		);
 	}
 }
