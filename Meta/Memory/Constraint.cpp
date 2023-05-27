0f0135d83a00a4631cef71c0c7e3c11a113b2f91
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-05-27 15:04:07 +0200

    [Memory] Reduced amount of used type traits

--- Meta/Memory/Constraint.cpp
+++ Meta/Memory/Constraint.cpp
@@ -14,7 +14,7 @@ namespace
 	auto constexpr
 		ByteAlign
 	=	::std::countr_zero
-		(	static_cast<::Meta::USize>
+		(	static_cast<USize>
 			(	BitSize
 				(	1_byte
 				)
@@ -22,6 +22,46 @@ namespace
 			)
 		)
 	+	1
+	;
+
+	// necessary to check unions
+	template
+		<	typename
+				t_tEntity
+		>
+	struct
+		Wrapper
+	{
+		[[no_unique_address]]
+		t_tEntity _;
+	};
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Alignment constexpr
+		Align_Of
+	{	::std::countr_zero
+		(	alignof
+			(	t_tEntity
+			)
+		)
+		+	ByteAlign
+	};
+
+
+	template
+		<	typename
+				t_tEntity
+		>
+	BitSize constexpr
+		Size_Of
+	=	ByteSize
+		{	sizeof
+			(	t_tEntity
+			)
+		}
 	;
 }
 
@@ -78,110 +118,9 @@ export namespace
 		>
 	Constraint constexpr
 		Constraint_Of
-	=	[]{	if	constexpr
-				(	::std::is_const_v
-					<	t_tEntity
-					>
-				or	::std::is_volatile_v
-					<	t_tEntity
-					>
-				)
-			{	return
-				Constraint_Of
-				<	::std::remove_cv_t
-					<	t_tEntity
-					>
-				>;
-			}
-			else
-			if	constexpr
-				(	::std::is_array_v
-					<	t_tEntity
-					>
-				)
-			{
-				auto
-					vConstraint
-				=	Constraint_Of
-					<	::std::remove_all_extents_t
-						<	t_tEntity
-						>
-					>
-				;
-
-				(	vConstraint.Size
-				*=	::std::extent_v
-					<	t_tEntity
-					>
-				);
-
-				return
-					vConstraint
-				;
-			}
-			else
-			if	constexpr
-				(	::std::is_reference_v
-					<	t_tEntity
-					>
-				)
-			{	return
-				Constraint_Of
-				<	::std::add_pointer_t
-					<	t_tEntity
-					>
-				>;
-			}
-			else
-			if	constexpr
-				(	::std::is_object_v
-					<	t_tEntity
-					>
-				)
-			{
-				// necessary to check unions
-				struct
-					Wrapper
-				{
-					[[no_unique_address]]
-					t_tEntity _;
-				};
-
-				if constexpr
-					(	not
-						::std::is_empty_v
-						<	Wrapper
-						>
-					)
-				{	return
-					Constraint
-					{	.	Align
-						=	{	::std::countr_zero
-								(	alignof
-									(	t_tEntity
-									)
-								)
-							+	ByteAlign
-							}
-					,	.	Size
-						=	ByteSize
-							{	sizeof
-								(	t_tEntity
-								)
-							}
-					};
-				}
-			}
-
-			return
-			Constraint
-			{	.Align
-				=	0_align
-			,	.Size
-				=	0_bit
-			};
-		}()
-	;
+	{	0_align
+	,	0_bit
+	};
 
 	template
 		<>
@@ -192,6 +131,293 @@ export namespace
 	{	1_align
 	,	1_bit
 	};
+
+	template
+		<>
+	Constraint constexpr
+		Constraint_Of
+		<	decltype(nullptr)
+		>
+	{	Align_Of<decltype(nullptr)>
+	,	Size_Of<decltype(nullptr)>
+	};
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+			*
+		>
+	{	Align_Of<t_tEntity*>
+	,	Size_Of<t_tEntity*>
+	};
+
+	template
+		<	typename
+				t_tMember
+		,	typename
+				t_tObject
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tMember
+			t_tObject
+			::*
+		>
+	{	Align_Of<t_tMember t_tObject::*>
+	,	Size_Of<t_tMember t_tObject::*>
+	};
+
+	template
+		<	typename
+				t_tEntity
+		>
+	requires
+	(	requires
+		{	sizeof(t_tEntity);
+			alignof(t_tEntity);
+		}
+	and	(	not
+			::std::is_class_v
+			<	t_tEntity
+			>
+		or	not
+			::std::is_empty_v
+			<	t_tEntity
+			>
+		)
+	and	(	not
+			::std::is_union_v
+			<	t_tEntity
+			>
+		or	not
+			::std::is_empty_v
+			<	Wrapper
+				<	t_tEntity
+				>
+			>
+		)
+	)
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+		>
+	{	Align_Of<t_tEntity>
+	,	Size_Of<t_tEntity>
+	};
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const
+		>
+	=	Constraint_Of
+		<	t_tEntity
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity volatile
+		>
+	=	Constraint_Of
+		<	t_tEntity
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const volatile
+		>
+	=	Constraint_Of
+		<	t_tEntity
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+				[]
+		>
+	{	Constraint_Of
+			<	t_tEntity
+			>
+		.	Align
+	,	0_bit
+	};
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const
+				[]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity volatile
+				[]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const volatile
+				[]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		,	USize
+				t_vExtent
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+				[	t_vExtent
+				]
+		>
+	{	Constraint_Of
+			<	t_tEntity
+			>
+		.	Align
+	,		Constraint_Of
+				<	t_tEntity
+				>
+			.	Size
+		*	t_vExtent
+	};
+
+	template
+		<	typename
+				t_tEntity
+		,	USize
+				t_vExtent
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const
+				[	t_vExtent
+				]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[	t_vExtent
+				]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		,	USize
+				t_vExtent
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity volatile
+				[	t_vExtent
+				]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[	t_vExtent
+				]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		,	USize
+				t_vExtent
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity const volatile
+				[	t_vExtent
+				]
+		>
+	=	Constraint_Of
+		<	t_tEntity
+				[	t_vExtent
+				]
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+			&
+		>
+	=	Constraint_Of
+		<	t_tEntity
+			*
+		>
+	;
+
+	template
+		<	typename
+				t_tEntity
+		>
+	Constraint constexpr
+		Constraint_Of
+		<	t_tEntity
+			&&
+		>
+	=	Constraint_Of
+		<	t_tEntity
+			*
+		>
+	;
 
 	template
 		<	typename
