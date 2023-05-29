165201c811099d96e2037ab91b2c337c6be1eb02
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-05-29 17:51:56 +0200

    [Evaluation] Use a function in memcheck.sh

--- Evaluation/memcheck.sh
+++ Evaluation/memcheck.sh
@@ -9,94 +9,29 @@ echo "Measuring memory consumption..."
 mkdir -p build/memcheck_$1_$2/
 cd build/memcheck_$1_$2/
 
-if [ $# -lt 3 ] || [ $3 == "virtual" ]
-then
-	echo "virtual"
+function execute_memcheck()
+{
+	echo $3
 	valgrind\
 		--tool=memcheck\
-		../Evaluation/Evaluation/virtual\
+		../Evaluation/bin/$3\
 		$1 $2\
-		2> virtual.txt
-fi
+		2> $3.txt
+}
 
-if [ $# -lt 3 ] || [ $3 == "any" ]
+if [ $# -lt 3 ]
 then
-	echo "any"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/any\
-		$1 $2\
-		2> any.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "dyno" ]
-then
-	echo "dyno"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/dyno\
-		$1 $2\
-		2> dyno.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "polymorphic" ]
-then
-	echo "polymorphic"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/polymorphic\
-		$1 $2\
-		2> polymorphic.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "type_erasure" ]
-then
-	echo "type_erasure"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/type_erasure\
-		$1 $2\
-		2> type_erasure.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "archetype" ]
-then
-	echo "archetype"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/archetype\
-		$1 $2\
-		2> archetype.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "replication" ]
-then
-	echo "replication"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/replication\
-		$1 $2\
-		2> replication.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "variant" ]
-then
-	echo "variant"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/variant\
-		$1 $2\
-		2> variant.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "visitor" ]
-then
-	echo "visitor"
-	valgrind\
-		--tool=memcheck\
-		../Evaluation/Evaluation/visitor\
-		$1 $2\
-		2> visitor.txt
+	execute_memcheck $1 $2 "any"
+	execute_memcheck $1 $2 "archetype"
+	execute_memcheck $1 $2 "dyno"
+	execute_memcheck $1 $2 "polymorphic"
+	execute_memcheck $1 $2 "replication"
+	execute_memcheck $1 $2 "type_erasure"
+	execute_memcheck $1 $2 "variant"
+	execute_memcheck $1 $2 "virtual"
+	execute_memcheck $1 $2 "visitor"
+else
+	execute_memcheck $1 $2 $3
 fi
 
 cd ../../
