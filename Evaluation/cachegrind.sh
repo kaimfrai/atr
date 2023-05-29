df65cc0cb620237c030a921c7eec2fdb8dd1bb3e
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-05-29 16:36:44 +0200

    [Evaluation] Use function in cachegrind.sh

--- Evaluation/cachegrind.sh
+++ Evaluation/cachegrind.sh
@@ -10,94 +10,31 @@ mkdir -p ./build/cachegrind_$1_$2/
 
 cd ./build/cachegrind_$1_$2/
 
-if [ $# -lt 3 ] || [ $3 == "virtual" ]
-then
-	echo "virtual"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/virtual\
-		$1 $2\
-		2> virtual.txt
-fi
+function execute_cachegrind ()
+{
+	echo $3
 
-if [ $# -lt 3 ] || [ $3 == "any" ]
-then
-	echo "any"
 	valgrind\
 		--tool=cachegrind\
-		../Evaluation/Evaluation/any\
+		../Evaluation/Evaluation/$3\
 		$1 $2\
-		2> any.txt
-fi
+		2> $3.txt
+}
 
-if [ $# -lt 3 ] || [ $3 == "dyno" ]
+if [ $# -lt 3 ]
 then
-	echo "dyno"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/dyno\
-		$1 $2\
-		2> dyno.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "polymorphic" ]
-then
-	echo "polymorphic"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/polymorphic\
-		$1 $2\
-		2> polymorphic.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "type_erasure" ]
-then
-	echo "type_erasure"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/type_erasure\
-		$1 $2\
-		2> type_erasure.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "archetype" ]
-then
-	echo "archetype"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/archetype\
-		$1 $2\
-		2> archetype.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "replication" ]
-then
-	echo "replication"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/replication\
-		$1 $2\
-		2> replication.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "variant" ]
-then
-	echo "variant"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/variant\
-		$1 $2\
-		2> variant.txt
-fi
-
-if [ $# -lt 3 ] || [ $3 == "visitor" ]
-then
-	echo "visitor"
-	valgrind\
-		--tool=cachegrind\
-		../Evaluation/Evaluation/visitor\
-		$1 $2\
-		2> visitor.txt
+	execute_cachegrind $1 $2 "virtual"
+	execute_cachegrind $1 $2 "any"
+	execute_cachegrind $1 $2 "dyno"
+	execute_cachegrind $1 $2 "polymorphic"
+	execute_cachegrind $1 $2 "type_erasure"
+	execute_cachegrind $1 $2 "archetype"
+	execute_cachegrind $1 $2 "replication"
+	execute_cachegrind $1 $2 "variant"
+	execute_cachegrind $1 $2 "visitor"
+	execute_cachegrind $1 $2 "virtual"
+else
+	execute_cachegrind $1 $2 $3
 fi
 
 cd ../../
