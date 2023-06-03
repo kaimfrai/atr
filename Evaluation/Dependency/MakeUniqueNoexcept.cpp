5a7aa042e2bbd9be36e0f84085fbcbe9842cd9ac
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-06-03 11:16:23 +0200

    [Evaluation] Add MakeUniqueNoexcept

---
+++ Evaluation/Dependency/MakeUniqueNoexcept.cpp
@@ -0,0 +1,29 @@
+export module Evaluation.Dependency.MakeUniqueNoexcept;
+
+import Std;
+
+export
+{
+	template
+		<	typename
+				t_tElement
+		>
+	auto constexpr
+	(	MakeUniqueNoexcept
+	)	()
+		noexcept
+	->	::std::unique_ptr<t_tElement>
+	{
+		t_tElement
+		*	aElement
+		=	new	(	::std::nothrow
+				)
+			t_tElement
+			{}
+		;
+		return
+		::std::unique_ptr
+		{	aElement
+		};
+	}
+}
