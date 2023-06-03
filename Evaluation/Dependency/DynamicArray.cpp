a29cf541b16ba7766cd1105d1af91c38ac458862
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-06-03 10:50:06 +0200

    [Evaluation] Add DynamicArray as a minimal noexcept drop-in for vector

---
+++ Evaluation/Dependency/DynamicArray.cpp
@@ -0,0 +1,122 @@
+export module Evaluation.Dependency.DynamicArray;
+
+import Std;
+
+export
+{
+	template
+		<	typename
+				t_tElement
+		>
+	class
+		DynamicArray
+	{
+		::std::byte
+		*	m_vBuffer
+		=	nullptr
+		;
+		::std::ptrdiff_t
+			m_vCount
+		=	0
+		;
+	public:
+		explicit(false) constexpr
+		(	DynamicArray
+		)	(	::std::size_t
+					i_vCapacity
+			)
+			noexcept
+		:	m_vBuffer
+			{	new	(	::std::nothrow
+					)
+				::std::byte
+					[	i_vCapacity
+					*	sizeof(t_tElement)
+					]
+			}
+		{}
+
+		explicit(false) constexpr
+		(	DynamicArray
+		)	(	DynamicArray const
+				&
+			)
+			noexcept
+		=	delete;
+
+		auto constexpr
+		(	operator=
+		)	(	DynamicArray const
+				&
+			)	&
+			noexcept
+		->	DynamicArray&
+		=	delete;
+
+		constexpr
+		(	compl
+			DynamicArray
+		)	()
+			noexcept
+		{
+			delete[]
+				m_vBuffer
+			;
+		}
+
+		auto constexpr
+		(	emplace_back
+		)	(	auto
+				&&	i_rArgument
+			)	&
+			noexcept
+		{
+			::std::construct_at
+			(	::std::bit_cast
+				<	t_tElement
+					*
+				>(	m_vBuffer
+				+	m_vCount
+				)
+			,	::std::forward<decltype(i_rArgument)>
+				(	i_rArgument
+				)
+			);
+			++	m_vCount
+			;
+		}
+
+		[[nodiscard]]
+		auto constexpr
+		(	begin
+		)	()	&
+			noexcept
+		->	t_tElement*
+		{	return
+			::std::launder
+			(	::std::bit_cast
+				<	t_tElement
+					*
+				>(	m_vBuffer
+				)
+			);
+		}
+
+		[[nodiscard]]
+		auto constexpr
+		(	end
+		)	()	&
+			noexcept
+		->	t_tElement*
+		{	return
+			::std::launder
+			(	::std::bit_cast
+				<	t_tElement
+					*
+				>(	m_vBuffer
+				+	m_vCount
+				)
+			);
+		}
+	};
+}
