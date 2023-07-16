456169825e58b9788b4034e421bed5d6cf4682fe
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2023-07-16 14:22:09 +0200

    [String] Add a rolling hash function
    This allows concatenation of hashes which have the same value as the hash
    of the concatenated strings.
    The distribution may still need some tweaking.

---
+++ Meta/String/Hash.cpp
@@ -0,0 +1,311 @@
+export module Meta.String.Hash;
+
+import Std;
+
+export namespace
+	Meta::String
+{
+	struct
+		Hash
+	{
+		::std::uint64_t
+			Value
+		{};
+
+		::std::uint64_t
+			Multiplier
+		{};
+
+		explicit(false) constexpr
+		(	Hash
+		)	()
+			noexcept
+		=	default;
+
+		explicit(true) constexpr
+		(	Hash
+		)	(	char const
+				*	i_aString
+			)
+			noexcept
+		:	Value{}
+		,	Multiplier
+			{	1u
+			}
+		{
+			::std::uint64_t static constexpr
+				vPerCharMultiplier
+			=	//	Value found by trial and error
+				//	Produces the best distribution for common chars
+				0x2102'C408'1020'4005
+			;
+
+			for	(
+				;	*	i_aString
+				;	++	i_aString
+				)
+			{
+				Value
+				*=	vPerCharMultiplier
+				;
+
+				Value
+				+=	static_cast<::std::uint64_t>
+					(	*	i_aString
+					)
+				bitand
+					//	Ensures case insensitivity
+					0b0101'1111
+				;
+
+				Multiplier
+				*=	vPerCharMultiplier
+				;
+			}
+		}
+
+		[[nodiscard]]
+		explicit(true) constexpr inline
+		(	operator bool
+		)	()	const
+			noexcept
+		{	return
+				Multiplier
+			>	0u
+			;
+		}
+
+		[[nodiscard]]
+		auto friend constexpr inline
+		(	operator not
+		)	(	Hash
+					i_vHash
+			)
+			noexcept
+		->	bool
+		{	return
+				not
+				static_cast<bool>
+				(	i_vHash
+				)
+			;
+		}
+
+		[[nodiscard]]
+		auto friend constexpr inline
+		(	operator+
+		)	(	Hash
+					i_vLeft
+			,	Hash
+					i_vRight
+			)
+			noexcept
+		->	Hash
+		{
+			i_vLeft
+			.	Value
+			*=	i_vRight
+				.	Multiplier
+			;
+			i_vLeft
+			.	Value
+			+=	i_vRight
+				.	Value
+			;
+			i_vLeft
+			.	Multiplier
+			*=	i_vRight
+				.	Multiplier
+			;
+			return
+				i_vLeft
+			;
+		}
+
+		[[nodiscard]]
+		auto friend constexpr inline
+		(	operator==
+		)	(	Hash
+			,	Hash
+			)
+			noexcept
+		->	bool
+		=	default;
+
+		[[nodiscard]]
+		auto constexpr inline
+		(	Fold64
+		)	()	const
+			noexcept
+		->	::std::uint64_t
+		{	return
+				Value
+			xor	Multiplier
+			;
+		}
+
+		[[nodiscard]]
+		auto constexpr inline
+		(	Fold32
+		)	()	const
+			noexcept
+		->	::std::uint32_t
+		{
+			::std::uint64_t static constexpr
+				vBitMask
+			=	0xFFFF'FFFF
+			;
+
+			::std::uint64_t const
+				vFold64
+			=	Fold64
+				()
+			;
+
+			auto const
+				vLowerBits
+			=		vFold64
+				bitand
+					vBitMask
+			;
+			auto const
+				vUpperBits
+			=	(	vFold64
+				bitand
+					compl
+					vBitMask
+				)
+			>>	32u
+			;
+			return
+				static_cast<::std::uint32_t>
+				(	vLowerBits
+				xor	vUpperBits
+				)
+			;
+		}
+
+		[[nodiscard]]
+		auto constexpr inline
+		(	Fold16
+		)	()	const
+			noexcept
+		->	::std::uint16_t
+		{
+			::std::uint32_t static constexpr
+				vBitMask
+			=	0xFFFF
+			;
+
+			::std::uint32_t const
+				vFold32
+			=	Fold32
+				()
+			;
+
+			auto const
+				vLowerBits
+			=		vFold32
+				bitand
+					vBitMask
+			;
+			auto const
+				vUpperBits
+			=	(	vFold32
+				bitand
+					compl
+					vBitMask
+				)
+			>>	16u
+			;
+			return
+				static_cast<::std::uint16_t>
+				(	vLowerBits
+				xor	vUpperBits
+				)
+			;
+		}
+
+		[[nodiscard]]
+		auto constexpr inline
+		(	Fold8
+		)	()	const
+			noexcept
+		->	::std::uint8_t
+		{
+			::std::uint16_t static constexpr
+				vBitMask
+			=	0xFF
+			;
+
+			::std::uint16_t const
+				vFold16
+			=	Fold16
+				()
+			;
+
+			auto const
+				vLowerBits
+			=		vFold16
+				bitand
+					vBitMask
+			;
+			auto const
+				vUpperBits
+			=	(	vFold16
+				bitand
+					compl
+					vBitMask
+				)
+			>>	8u
+			;
+			return
+				static_cast<::std::uint8_t>
+				(	vLowerBits
+				xor	vUpperBits
+				)
+			;
+		}
+
+		[[nodiscard]]
+		auto constexpr inline
+		(	Fold4
+		)	()	const
+			noexcept
+		->	::std::uint8_t
+		{
+			::std::uint8_t static constexpr
+				vBitMask
+			=	0x0F
+			;
+
+			::std::uint8_t const
+				vFold8
+			=	Fold8
+				()
+			;
+
+			auto const
+				vLowerBits
+			=		vFold8
+				bitand
+					vBitMask
+			;
+			auto const
+				vUpperBits
+			=	(	vFold8
+				bitand
+					compl
+					vBitMask
+				)
+			>>	4u
+			;
+			return
+				static_cast<::std::uint8_t>
+				(	vLowerBits
+				xor	vUpperBits
+				)
+			;
+		}
+	};
+}
---
+++ Test/Meta/String/Hash.cpp
@@ -0,0 +1,284 @@
+import Meta.String.Hash;
+
+import Std;
+
+using ::Meta::String::Hash;
+
+static_assert
+(	Hash("").Value
+==	0u
+);
+static_assert
+(	Hash("")
+);
+static_assert
+(	not
+	Hash()
+);
+
+[[nodiscard]]
+auto constexpr inline
+(	AssertHashEqual
+)	(	char const
+		*	i_aLeft
+	,	char const
+		*	i_aRight
+	)
+	noexcept
+->	bool
+{
+	if	(	Hash(i_aLeft)
+		!=	Hash(i_aRight)
+		)
+	{	::std::unreachable
+		();
+	}
+
+	return
+		true
+	;
+}
+
+[[nodiscard]]
+auto constexpr inline
+(	CheckCaseInsensitivity
+)	()
+	noexcept
+->	bool
+{
+	for	(	char
+				vLeftChar
+			=	'@'
+			,	vRightChar
+			=	'`'
+		;	(	vLeftChar
+			<= '_'
+			)
+		;	++	vLeftChar
+		,	++	vRightChar
+		)
+	{
+		char const
+			vLeftString
+			[]
+		{	vLeftChar
+		,	'\0'
+		};
+		char const
+			vRightString
+			[]
+		{	vRightChar
+		,	'\0'
+		};
+		if	(	not
+				AssertHashEqual
+				(	vLeftString
+				,	vRightString
+				)
+			)
+		{	return
+				false
+			;
+		}
+	}
+	return
+		true
+	;
+}
+static_assert
+(	CheckCaseInsensitivity
+	()
+,	"Case insensitivity violated!"
+);
+
+static_assert
+(	Hash("ABC")
+!=	Hash("ABCDEF")
+);
+static_assert
+(	Hash("ABC")
+!=	Hash("DEF")
+);
+static_assert
+(	(	Hash("ABC")
+	+	Hash("DEF")
+	)
+==	Hash("ABCDEF")
+);
+
+static_assert
+(	(	Hash("abcdefghi")
+	+	Hash("jklmnopq")
+	+	Hash("rstuvwxyz")
+	)
+==	Hash("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+);
+
+template
+	<	::std::size_t
+			t_vStringLength
+	>
+[[nodiscard]]
+auto constexpr inline
+(	AddDistributionEntry
+)	(	int
+		(&	o_rStorage
+		)	[	16uz
+			]
+	,	char const
+		(&	i_rString
+		)	[	t_vStringLength
+			]
+	)
+	noexcept
+->	bool
+{
+	int
+	&	rDistributionCount
+	=	o_rStorage
+		[	Hash(i_rString)
+			.	Fold4
+				()
+		]
+	;
+	if	(	rDistributionCount
+		>	// Best observed value so far
+			4125
+		)
+	{	return
+			false
+		;
+	}
+
+	++	rDistributionCount
+	;
+
+	return
+		true
+	;
+}
+
+template
+	<	::std::size_t
+			t_vCharacterCount
+	,	::std::size_t
+			t_vStringLength
+	>
+[[nodiscard]]
+auto constexpr inline
+(	DistributePermutations
+)	(	int
+		(&	i_rStorage
+		)	[	16uz
+			]
+	,	char const
+		(&	i_rCharacters
+		)	[	t_vCharacterCount
+			]
+	,	char
+		(&	i_rString
+		)	[	t_vStringLength
+			]
+	,	::std::size_t
+			i_vIndex
+	)
+	noexcept
+->	bool
+{
+	if	(	not
+			AddDistributionEntry
+			(	i_rStorage
+			,	i_rString
+			)
+		)
+	{	return
+			false
+		;
+	}
+
+	if	(	i_vIndex
+		<	(	t_vStringLength
+			-	1uz
+			)
+		)
+	{
+		for	(	char
+					vChar
+			:	i_rCharacters
+			)
+		{
+			i_rString
+				[	i_vIndex
+				]
+			=	vChar
+			;
+
+			if	(	not
+					DistributePermutations
+					(	i_rStorage
+					,	i_rCharacters
+					,	i_rString
+					,	(	i_vIndex
+						+	1uz
+						)
+					)
+				)
+			{	return
+					false
+				;
+			}
+		}
+		i_rString
+			[	i_vIndex
+			]
+		=	'\0'
+		;
+	}
+
+	return
+		true
+	;
+}
+
+[[nodiscard]]
+auto constexpr inline
+(	CheckDistribution
+)	()
+	noexcept
+->	bool
+{
+	static char constexpr
+		vRelevantCharacters
+		[]
+	{	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+	,	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
+	,	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'
+	,	'U', 'V', 'W', 'X', 'Y', 'Z', '_', '.', '[', ']'
+	};
+
+	int
+		vDistributionCounts
+		[	16uz
+		]
+	{};
+	char
+		vString
+		[	4uz
+		]
+	{};
+
+	return
+		DistributePermutations
+		(	vDistributionCounts
+		,	vRelevantCharacters
+		,	vString
+		,	0uz
+		)
+	;
+}
+
+static_assert
+(	CheckDistribution
+	()
+,	"Hash distribution has become worse!"
+);
