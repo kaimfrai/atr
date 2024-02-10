c0bec1ce38b7ddd7367f0510f11399a6a2ec513f
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2024-02-10 21:20:23 +0100

    [Meta] Add missing SIMD files
    Files missing from previous commit

---
+++ Meta/Auto/Simd/Cast.cpp
@@ -0,0 +1,91 @@
+module;
+
+#include <immintrin.h>
+
+export module Meta.Auto.Simd.Cast;
+
+import Meta.Auto.Simd.Float;
+import Meta.Auto.Simd.UInt32;
+import Meta.Auto.Simd.UInt8;
+
+import Std;
+
+export namespace
+	Meta
+{
+	template
+		<	typename
+				t_tTarget
+		>
+	struct
+		SimdCastFunction
+	{};
+
+	template
+		<>
+	struct
+		SimdCastFunction
+		<	float
+		>
+	{
+		[[nodiscard]]
+		auto static constexpr inline
+		(	operator()
+		)	(	Simd<::std::uint32_t[8uz]>
+					i_vSource
+			)
+			noexcept
+		->	Simd<float[8uz]>
+		{	return
+			{	.	m_vRaw
+				=	_mm256_cvtepi32_ps
+					(	i_vSource
+						.	m_vRaw
+					)
+			};
+		}
+	};
+
+	template
+		<>
+	struct
+		SimdCastFunction
+		<	::std::uint32_t
+		>
+	{
+		[[nodiscard]]
+		auto static constexpr inline
+		(	operator()
+		)	(	Simd<::std::uint8_t[8uz]>
+					i_vSource
+			)
+			noexcept
+		->	Simd<::std::uint32_t[8uz]>
+		{
+			::std::uint64_t
+				vValue
+				[	2uz
+				]
+			{	i_vSource
+				.	m_vRaw
+			};
+
+			return
+			{	.	m_vRaw
+				=	_mm256_cvtepu8_epi32
+					(	::std::bit_cast<__m128i>
+						(	vValue
+						)
+					)
+			};
+		}
+	};
+
+	template
+		<	typename
+				t_tElement
+		>
+	SimdCastFunction<t_tElement> constexpr inline
+		SimdCast
+	{};
+}
---
+++ Meta/Auto/Simd/Fill.cpp
@@ -0,0 +1,94 @@
+module;
+
+#include <immintrin.h>
+
+export module Meta.Auto.Simd.Fill;
+
+import Meta.Auto.Simd.Float;
+import Meta.Auto.Simd.UInt32;
+import Meta.Auto.Simd.UInt8;
+
+import Std;
+
+export namespace
+	Meta
+{
+	template
+		<	typename
+				t_tTarget
+		>
+	struct
+		SimdFillFunction
+	{};
+
+	template
+		<>
+	struct
+		SimdFillFunction
+		<	float
+				[	8uz
+				]
+		>
+	{
+		[[nodiscard]]
+		auto static inline
+		(	operator()
+		)	(	float
+					i_vSource
+			)
+			noexcept
+		->	Simd<float[8uz]>
+		{
+			return
+			{	.	m_vRaw
+				=	_mm256_broadcast_ss
+					(	&	i_vSource
+					)
+			};
+		}
+	};
+
+	template
+		<>
+	struct
+		SimdFillFunction
+		<	::std::uint32_t
+				[	8uz
+				]
+		>
+	{
+		[[nodiscard]]
+		auto static inline
+		(	operator()
+		)	(	::std::uint32_t
+					i_vSource
+			)
+			noexcept
+		->	Simd<::std::uint32_t[8uz]>
+		{
+			::std::uint32_t
+				vSource
+				[	4uz
+				]
+			{	i_vSource
+			};
+
+			return
+			{	.	m_vRaw
+				=	_mm256_broadcastd_epi32
+					(	::std::bit_cast<__m128i>
+						(	vSource
+						)
+					)
+			};
+		}
+	};
+
+	template
+		<	typename
+				t_tElement
+		>
+	SimdFillFunction<t_tElement> constexpr inline
+		SimdFill
+	{};
+}
