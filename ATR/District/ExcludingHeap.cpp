0be9983656fde0ad4984732220d30e27a5848eb6
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2024-04-01 15:43:22 +0200

    [ATR] Move DistrictIndexOf to ATR.District.Info

--- ATR/District/ExcludingHeap.cpp
+++ ATR/District/ExcludingHeap.cpp
@@ -3,10 +3,6 @@ export module ATR.District.ExcludingHeap;
 import ATR.District.Info;
 import ATR.District.MoveHeapGuard;
 
-import ATR.Member.AlignBuffer;
-import ATR.Member.CountedType;
-import ATR.Member.LayoutList;
-
 import Meta.String.Hash;
 import Meta.Token.Type;
 import Meta.Token.TypeID;
@@ -15,61 +11,10 @@ using ::Meta::String::Hash;
 using ::Meta::String::ImplicitHash;
 using ::Meta::Type;
 using ::Meta::TypeID;
-using ::Meta::TypeToken;
 
 export namespace
 	ATR::District
 {
-	template
-		<	template
-				<	typename
-				,	typename
-					...
-				>
-			typename
-				t_t1Instance
-		,	typename
-				t_tTypeName
-		,	typename
-			...	t_tpDistrict
-		>
-	[[nodiscard]]
-	auto constexpr inline
-	(	DistrictIndexOf
-	)	(	TypeToken<t_t1Instance<t_tTypeName, t_tpDistrict...>>
-		,	TypeID
-				i_vDistrict
-		)
-		noexcept
-	->	short
-	{
-		TypeID static constexpr
-			vDistrictList
-			[]
-		{	Type<t_tpDistrict>
-			...
-		};
-
-		for	(	short
-					vDistrictIndex
-				=	0
-			;
-			;	++	vDistrictIndex
-			)
-		{	if	(	vDistrictList
-					[	vDistrictIndex
-					]
-				==	i_vDistrict
-				)
-			{	return
-					vDistrictIndex
-				+	1
-				;
-			}
-		}
-	}
-
-
 	template
 		<	Info
 				t_vInfo
--- ATR/District/Info.cpp
+++ ATR/District/Info.cpp
@@ -1,8 +1,13 @@
 export module ATR.District.Info;
 
 import Meta.String.Hash;
+import Meta.Token.Type;
+import Meta.Token.TypeID;
 
 using ::Meta::String::ImplicitHash;
+using ::Meta::Type;
+using ::Meta::TypeID;
+using ::Meta::TypeToken;
 
 export namespace
 	ATR::District
@@ -17,4 +22,53 @@ export namespace
 			Host
 		{};
 	};
+
+	template
+		<	template
+				<	typename
+				,	typename
+					...
+				>
+			typename
+				t_t1Instance
+		,	typename
+				t_tTypeName
+		,	typename
+			...	t_tpDistrict
+		>
+	[[nodiscard]]
+	auto constexpr inline
+	(	DistrictIndexOf
+	)	(	TypeToken<t_t1Instance<t_tTypeName, t_tpDistrict...>>
+		,	TypeID
+				i_vDistrict
+		)
+		noexcept
+	->	short
+	{
+		TypeID static constexpr
+			vDistrictList
+			[]
+		{	Type<t_tpDistrict>
+			...
+		};
+
+		for	(	short
+					vDistrictIndex
+				=	0
+			;
+			;	++	vDistrictIndex
+			)
+		{	if	(	vDistrictList
+					[	vDistrictIndex
+					]
+				==	i_vDistrict
+				)
+			{	return
+					vDistrictIndex
+				+	1
+				;
+			}
+		}
+	}
 }
