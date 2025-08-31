c814024f3af90606b93f44e2f9d7abee41f863af
Kai Müller <119608303+kaimfrai@users.noreply.github.com>
2025-08-31 15:45:08 +0200

    [Evaluation] Post-process perf for better diffs

--- Evaluation/perf.sh
+++ Evaluation/perf.sh
@@ -8,18 +8,31 @@ echo "Measuring average execution time over $2 executions..."
 
 mkdir -p Evaluation/Results/perf_$1/
 
-echo 0 > /proc/sys/kernel/nmi_watchdog
-
 function execute_perf()
 {
 	echo $3
 
+	stat_file=Evaluation/Results/perf_$1/$3.txt
 	perf\
 		stat\
 		--repeat $2\
 		build/Evaluation/Speed/bin/$3\
 		0 $1\
-		2> Evaluation/Results/perf_$1/$3.txt
+		2> $stat_file
+
+	# replace multiple spaces with tabs
+	sed -i 's/  \+/\t/g' $stat_file
+	sed -i 's/msec /msec\t/g' $stat_file
+	# single colums
+	sed -i 's/\t#\t/\n\t/g' $stat_file
+	# header then indendet value
+	sed -i 's/^\t\([^\t]\+\)\t\([^(]\+\)/\2\n\t\1/g' $stat_file
+	# remove trailing whitespace
+	sed -i 's/\t$//g' $stat_file
+	# new line for +-
+	sed -i 's/\t*( +-\t\([0-9,%]\+\) )/\n\t+- \1/g' $stat_file
+	# remove :u
+	sed -i 's/:u$//g' $stat_file
 }
 
 if	[ $# -lt 3 ] \
@@ -41,7 +54,5 @@ then
 else
 	execute_perf $1 $2 $3
 fi
-
-echo 1 > /proc/sys/kernel/nmi_watchdog
 
 echo "Results in Evaluation/Results/perf_$1/"
